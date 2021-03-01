906ecc813e1195d485bcf5a9010f9d101d8cec25
Staneesh <blkreasons@outlook.com>
2021-03-01 12:49:20 +0100

    Initial commit, preparing the repository

---
+++ compile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+error_message="Provide debug/release arguments to this script!"
+
+if [ $# -lt 1 ] || [ $# -gt 1 ] 
+then
+	echo $error_message 
+	exit 1
+fi
+
+mode=$1
+flags=""
+common="-lm -pthread -march=native -lSDL2 -lSDL2main"
+
+if [ $mode == "debug" ]
+then
+	flags="-O0 -Wall -Wextra -Wshadow -g -fsanitize=undefined"
+elif [ $mode == "release" ]
+then
+	flags="-O3"
+else
+	echo $error_message
+	exit 1
+fi
+
+mkdir -p build 
+mkdir -p build/debug 
+mkdir -p build/release 
+
+time gcc source/main.c -o build/"$mode"/main $flags $common 
+
+# TODO(stanisz): i dont know why these are created, so this
+# deletion is a temporary hotfix
+rm debug release > /dev/null 2>$1
+
+# NOTE(stanisz): generate tags for vim (requires ctags installed)
+ctags -R
